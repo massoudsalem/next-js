ae7a69e24e1ebb35c203800eddbcbddaef0ec228
Mohammad Massoud <36207910+massoudsalem@users.noreply.github.com>
2023-07-31 10:23:40 +0300

    add: Chapter4 (SSR, SSG, ISR)

---
+++ Next13+ Dave Gray/Chapter4/next04/app/users/[userId]/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+        <>
+            <h2>Not found</h2>
+            <br />
+            <p>This user does not exist.</p>
+        </>
+    )
+}
---
+++ Next13+ Dave Gray/Chapter4/next04/app/users/[userId]/page.tsx
@@ -0,0 +1,65 @@
+import getUser from "@/lib/getUser"
+import getUserPosts from "@/lib/getUserPosts"
+import { Suspense } from "react"
+import UserPosts from "./components/UserPosts"
+import type { Metadata } from 'next'
+import { notFound } from 'next/navigation'
+import getAllUsers from "@/lib/getAllUsers"
+
+type Params = {
+    params: {
+        userId: string
+    }
+}
+
+export async function generateMetadata({ params: { userId } }: Params): Promise<Metadata> {
+    const userData: Promise<User> = getUser(userId)
+    const user: User = await userData
+
+    if (!user.name) {
+        return {
+            title: 'User not found',
+            description: 'User not found',
+        }
+    }
+
+    return {
+        title: user.name,
+        description: `This is the page of ${user.name}`
+    }
+
+}
+
+export default async function UserPage({ params: { userId } }: Params) {
+    const userData: Promise<User> = getUser(userId)
+    const userPostsData: Promise<Post[]> = getUserPosts(userId)
+
+    // If not progressively rendering with Suspense, use Promise.all
+    //const [user, userPosts] = await Promise.all([userData, userPostsData])
+
+    const user = await userData
+
+    if (!user.name) {
+        return notFound()
+    }
+
+    return (
+        <>
+            <h2>{user.name}</h2>
+            <br />
+            <Suspense fallback={<h2>Loading...</h2>}>
+                {/* @ts-expect-error Server Component */}
+                <UserPosts promise={userPostsData} />
+            </Suspense>
+        </>
+    )
+}
+
+export async function generateStaticParams() {
+    const users: Promise<User[]> = getAllUsers()
+    const usersData: User[] = await users
+
+    return usersData.map((user) => ({
+        userId: user.id.toString()
+    }))
+}
